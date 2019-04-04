17ccd703da93f0d2f2400d4f847eac827623bb18
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-04-04 00:31:43 -0300

    Add axis labels and prints

--- simulacao_tanque/simulacao_tanque_1.py
+++ simulacao_tanque/simulacao_tanque_1.py
@@ -7,7 +7,7 @@ h0 = 0          # altura inicial
 h_t = []        # altura em instante de tempo "t"
 
 # Entrada tanque
-ai = 0.5        # abertura de entrada
+ai = 1        # abertura de entrada
 Ki = 100        # constante de abertura de entrada
 Fi = ai * Ki    # vazão de entrada
 
@@ -20,11 +20,21 @@ stop = 120      # tempo de simulacao
 step = 1        # intervalo de tempo entre amostras ( "delta t" )
 t = np.linspace(0,stop,stop)
 
+h_j = 0
+h_i = 0
+
 for i in range(len(t)):
+    h_j = h0
     h = h0 + (1/A) * (Fi - ( Fo * h0 )) * step
+    h_i = h
+    if h_i == h_j:
+        print("Nível de equilíbrio: " + str(h_i))
+        print("Instante: " + str(i))
     h0 = h
     h_t.append(h)
 
 plt.plot(t,h_t)
+plt.xlabel("Tempo")
+plt.ylabel("Nível da coluna do líquido")
 plt.grid()
 plt.show()
